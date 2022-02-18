8a7f8871b9f3944aa0c940b86c0fc768459daddf
Ivan Barrera <guillermoivan@protonmail.com>
2022-02-18 13:56:27 -0300

    Estilos + Nueva Pagina

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -7,7 +7,10 @@ import "../../css/movies.css"
 export default function Movies() {
   // movies es la coleccion de pelis en el objeto moviesState
   const {moviesState:{movies}} = useContext(moviesContext)
-  const moviesReducido= [...movies].slice(0,5)
+  /*Podria usar el link(Si viene de Home, o de MovieList) para limitar el array o dejarlo por defecto*/
+  console.log(typeof window.location.pathname);
+  
+  const moviesReducido=  window.location.pathname=="/" ? [...movies].slice(0,5) : movies;
   return (
     <section className="movies margin">
       {moviesReducido.map((eachMovie) => {
--- src/components/movieDetails/Detalles.jsx
+++ src/components/movieDetails/Detalles.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
 import '../../css/detalles.css'
 export default function Detalles({ movie, raiting }) {
 
