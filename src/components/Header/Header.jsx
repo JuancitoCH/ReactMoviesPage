6eb727369b8f4cd15218e3ea1b0131b2ec43f531
juancitoCH <cjuan.chona@hotmail.com>
2022-02-21 01:56:14 -0300

    busqueda por el header pero solo muestra en consola

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import '../../css/Header.css'
 import { BiCameraMovie } from 'react-icons/bi';
+import {moviesContext} from '../../context/MoviesContext'
 // import {useHistory} from 'react-router-dom'
 // BiCameraMovie
 import {Link} from 'react-router-dom'
 import LogOut from './LogOut';
+import { useContext } from 'react';
 
 export default function Header() {
+  
+  const barra = useRef()
+  const {moviesState:{movies}} = useContext(moviesContext)
+  console.log(movies)
+  const buscar=()=>{
+    console.log(barra.current.value)
+    movies.forEach(movie => {
+      let titulo = movie.Title.toLowerCase()
+      let input = barra.current.value.toLowerCase()
+      if(titulo.indexOf(input)!==-1){
+        console.log(movie)
+      }
+    });
+  }
 
   return (
     <div className="Header dark" id='Header'>
@@ -16,8 +32,8 @@ export default function Header() {
       </Link>
 
       <div className="Header_divBotonBusqueda">
-        <input type="text" className="Header_divBotonBusqueda_barraBusqueda" />
-        <button className="Header_divBotonBusqueda_button">
+        <input type="text" ref={barra} className="Header_divBotonBusqueda_barraBusqueda" />
+        <button className="Header_divBotonBusqueda_button" onClick={buscar}>
           <img
             src="/lupaicon.svg"
             className="Header_divBotonBusqueda_button_img"
